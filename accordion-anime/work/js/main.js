1ee3fea92261412f2ab09e8938daa37b1b193464
tetsu-takumi <detsu.0113.soccer@icloud.com>
2025-07-03 23:14:48 +0900

    アニメーション追加１

--- accordion-anime/work/js/main.js
+++ accordion-anime/work/js/main.js
@@ -1,1 +1,18 @@
-const sclole = document.getElementById(list-txt1);
+const items = document.querySelectorAll(".list-item");
+
+items.forEach((item) => {
+  const content = item.querySelector(".list-content");
+
+  item.addEventListener("click", () => {
+    const isOpen = item.classList.contains("active");
+
+    if (isOpen) {
+      content.style.height = "0px";
+      item.classList.remove("active");
+    } else {
+      const contentHeight = content.scrollHeight;
+      content.style.height = contentHeight + "px";
+      item.classList.add("active");
+    }
+  });
+});
